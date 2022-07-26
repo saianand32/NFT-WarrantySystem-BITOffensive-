4bd6d962312acf46d28886e1d939516dd5305ada
nitish kumar <kumarnitish072001@gmail.com>
2022-07-27 02:47:05 +0530

    yy

--- firstimp-client22/src/index.js
+++ firstimp-client22/src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-<<<<<<< HEAD
 import "./style.scss";
-=======
-import "./style.scss"
->>>>>>> 8443316689e8b4d70f79a5bd93f6226c21611028
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
@@ -15,21 +11,11 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
-<<<<<<< HEAD
-// import { Web3ReactProvider } from "@web3-react/core";
-// import Web3 from "web3";
-
-// function getLibrary(provider) {
-//   return new Web3(provider);
-// }
-=======
->>>>>>> 8443316689e8b4d70f79a5bd93f6226c21611028
 
 // store
 const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-<<<<<<< HEAD
   <React.StrictMode>
    
       <Provider store={store}>
@@ -47,19 +33,4 @@ ReactDOM.render(
 );
 
 
-=======
-  // <React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  // </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
->>>>>>> 8443316689e8b4d70f79a5bd93f6226c21611028
 serviceWorker.unregister();
