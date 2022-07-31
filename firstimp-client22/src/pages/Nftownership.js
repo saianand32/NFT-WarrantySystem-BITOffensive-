3552b8818ae3d4a8a42d1a2dc6da050c60bc90cc
SAISHWAR ANAND <saishwaranand32@gmail.com>
2022-07-31 11:40:39 +0530

    added nftOwnership page2

--- firstimp-client22/src/pages/Nftownership.js
+++ firstimp-client22/src/pages/Nftownership.js
@@ -13,12 +13,14 @@ const Nftownership = () => {
   const [userNft, setUserNfts] = useState([]);
   console.log(address);
 
+ 
   const fetchNFTs = async () => {
     console.log("jscbj");
     const apiKey = "B5SSMRob-FxmkyHMWaRl2GOBc3H7PcoS";
     const baseURL = `https://eth-rinkeby.alchemyapi.io/nft/v2/${apiKey}/getNFTs/`;
 
     const ownerAddr = "0x9961c76F13CE42fA748E5d758EDdeB5995916aD7";
+    
 
     var config = {
       method: "get",
@@ -53,31 +55,33 @@ const Nftownership = () => {
           
           </div>
 
+ 
+      <div style={{display:"flex"}}>
           
       {userNft.map((p, i) => (
-        <div key={i}>
+        <div key={i} className="nftcontain"  >
         
-          <div class="nft">
-            <div class="main">
-              <img class="tokenImage" src={p.metadata.image} alt="NFT" />
+          <div className="nft">
+            <div className="main">
+              <img className="tokenImage" src={p.metadata.image} alt="NFT" />
 
               <div>
                 {p.metadata.properties.map((prop, k) => (
                   <div key={k}>
                     <div>
-                      <p class="description">{prop.type}:</p>:{prop.value}
+                      <p className="description">{prop.type}:</p>:{prop.value}
                     </div>
                   </div>
                 ))}
               </div>
 
            
-              <div class="tokenInfo">
-                <div class="price">
+              <div className="tokenInfo">
+                <div className="price">
                   <ins>◘</ins>
                   <p></p>
                 </div>
-                <div class="duration">
+                <div className="duration">
                   <ins>◷</ins>
                   <p>11 days left</p>
                 </div>
@@ -88,6 +92,7 @@ const Nftownership = () => {
           </div>
         </div>
       ))}
+      </div>
     </div>
   );
 };
